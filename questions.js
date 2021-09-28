a80b1a3d7505e9d1bbebfca12c2709c81ff32363
Quark <72112478+QUARK88@users.noreply.github.com>
2021-09-28 09:38:22 -0400

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,10 +1,10 @@
 questions =  [
-    {"id": 0, "question": "Right.", "effects": {"right": 1.41421356237}}
-    {"id": 1, "question": "Left.", "effects": {"right": -1.41421356237}}
-    {"id": 2, "question": "Auth.", "effects": {"auth": 1.41421356237}}
-    {"id": 3, "question": "Lib.", "effects": {"auth": -1.41421356237}}
-    {"id": 4, "question": "AuthRight.", "effects": {"auth": 1, "right": 1}}
-    {"id": 5, "question": "LibRight.", "effects": {"auth": -1, "right": 1}}
-    {"id": 6, "question": "AuthLeft.", "effects": {"auth": 1, "right": -1}}
+    {"id": 0, "question": "Right.", "effects": {"right": 1.41421356237}},
+    {"id": 1, "question": "Left.", "effects": {"right": -1.41421356237}},
+    {"id": 2, "question": "Auth.", "effects": {"auth": 1.41421356237}},
+    {"id": 3, "question": "Lib.", "effects": {"auth": -1.41421356237}},
+    {"id": 4, "question": "AuthRight.", "effects": {"auth": 1, "right": 1}},
+    {"id": 5, "question": "LibRight.", "effects": {"auth": -1, "right": 1}},
+    {"id": 6, "question": "AuthLeft.", "effects": {"auth": 1, "right": -1}},
     {"id": 7, "question": "LibLeft.", "effects": {"auth": -1, "right": -1}}
 ];
